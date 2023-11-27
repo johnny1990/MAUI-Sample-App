718ac7faaa9d85a1d277a952df5a7067b4002995
Man Ioan <31920272+johnny1990@users.noreply.github.com>
2023-11-28 00:36:53 +0200

    Add project files.

---
+++ MAUISampleApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MAUISampleApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MAUISampleApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MAUISampleApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
