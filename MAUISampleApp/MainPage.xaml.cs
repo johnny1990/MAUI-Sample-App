90ce0421aa439115ff3e624b65b1976135496397
Man Ioan <31920272+johnny1990@users.noreply.github.com>
2023-11-29 20:58:45 +0200

    Implement Sample Calculator  MAUI

--- MAUISampleApp/MainPage.xaml.cs
+++ MAUISampleApp/MainPage.xaml.cs
@@ -1,25 +1,129 @@
-﻿namespace MAUISampleApp
+﻿using MAUISampleApp.Models;
+
+namespace MAUISampleApp
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-
         public MainPage()
         {
             InitializeComponent();
+            ClearCalculator(this, null);
+
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        string currentEntry = "";
+        int currentState = 1;
+        string mathOperator;
+        double firstNumber, secondNumber;
+        string decimalFormat = "N0";
+
+
+
+        void SelectNumber(object sender, EventArgs e)
         {
-            count++;
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
+            Button button = (Button)sender;
+            string pressed = button.Text;
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            currentEntry += pressed;
+
+            if ((this.resultText.Text == "0" && pressed == "0")
+                || (currentEntry.Length <= 1 && pressed != "0")
+                || currentState < 0)
+            {
+                this.resultText.Text = "";
+                if (currentState < 0)
+                    currentState *= -1;
+            }
+
+            if (pressed == "." && decimalFormat != "N2")
+            {
+                decimalFormat = "N2";
+            }
+
+            this.resultText.Text += pressed;
+        }
+
+        void SelectOperator(object sender, EventArgs e)
+        {
+            LockNumberValue(resultText.Text);
+
+            currentState = -2;
+            Button button = (Button)sender;
+            string pressed = button.Text;
+            mathOperator = pressed;
+        }
+
+        private void LockNumberValue(string text)
+        {
+            double number;
+            if (double.TryParse(text, out number))
+            {
+                if (currentState == 1)
+                {
+                    firstNumber = number;
+                }
+                else
+                {
+                    secondNumber = number;
+                }
+
+                currentEntry = string.Empty;
+            }
+        }
+
+        void ClearCalculator(object sender, EventArgs e)
+        {
+            firstNumber = 0;
+            secondNumber = 0;
+            currentState = 1;
+            decimalFormat = "N0";
+            this.resultText.Text = "0";
+            currentEntry = string.Empty;
+        }
+
+        void CalculateComp(object sender, EventArgs e)
+        {
+            if (currentState == 2)
+            {
+                if (secondNumber == 0)
+                    LockNumberValue(resultText.Text);
+
+                double result = Calculator.CalculateOperationType(firstNumber, secondNumber, mathOperator);
+
+                this.CurrentCalculation.Text = $"{firstNumber} {mathOperator} {secondNumber}";
+
+                this.resultText.Text = result.ExtToTrimmedString(decimalFormat);
+                firstNumber = result;
+                secondNumber = 0;
+                currentState = -1;
+                currentEntry = string.Empty;
+            }
+        }
+
+        void NegativeComp(object sender, EventArgs e)
+        {
+            if (currentState == 1)
+            {
+                secondNumber = -1;
+                mathOperator = "×";
+                currentState = 2;
+                CalculateComp(this, null);
+            }
+        }
+
+        void PercentageComp(object sender, EventArgs e)
+        {
+            if (currentState == 1)
+            {
+                LockNumberValue(resultText.Text);
+                decimalFormat = "N2";
+                secondNumber = 0.01;
+                mathOperator = "×";
+                currentState = 2;
+                CalculateComp(this, null);
+            }
         }
     }
-
 }
+
---
+++ MAUISampleApp/Models/CalculatorOperations.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MAUISampleApp.Models
+{
+    public static class Calculator
+    {
+        public static double CalculateOperationType(double val1, double val2, string sign)
+        {
+            double result = 0;
+
+            switch (sign)
+            {
+                case "÷":
+                    result = val1 / val2;
+                    break;
+                case "×":
+                    result = val1 * val2;
+                    break;
+                case "+":
+                    result = val1 + val2;
+                    break;
+                case "-":
+                    result = val1 - val2;
+                    break;
+            }
+
+            return result;
+        }
+
+        //???
+        public static string ExtToTrimmedString(this double target, string decimalFormat)
+        {
+            string strValue = target.ToString(decimalFormat); //Get the stock string
+
+            //If there is a decimal point present
+            if (strValue.Contains("."))
+            {
+                //Remove all trailing zeros
+                strValue = strValue.TrimEnd('0');
+
+                //If all we are left with is a decimal point
+                if (strValue.EndsWith(".")) //then remove it
+                    strValue = strValue.TrimEnd('.');
+            }
+
+            return strValue;
+        }
+    }
+}
